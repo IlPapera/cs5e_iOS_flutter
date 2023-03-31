d7de43f9a323316c85d4a6d3b12822d868dcf1fa
IlPapera <parhamasadbigi10@gmail.com>
2023-03-31 03:48:23 +0200

    finished up the stats part

--- cs5e/lib/main.dart
+++ cs5e/lib/main.dart
@@ -60,28 +60,52 @@ final List<String> keys = [
   '47-currSpeed',
 ];
 
+Text randomChild = const Text('');
+
+int level = 2;
+
+String nomePersonaggio = 'Pallad';
+
 void main() {
   runApp(const MyApp());
 }
 
+int profBonusFromLevel(int level) {
+  return 2;
+}
+
+int modifierFromValue(int value) {
+  int modifier = (((value - value % 2) - 10) / 2).round();
+
+  return modifier;
+}
+
+String valueToSignedString(int value) {
+  return value > 0 ? '+${value.toString()}' : value.toString();
+}
+
 Widget attributeBoxGenerator(
-    String name, int value, List<String> stats, List<int> profs) {
+    String name, int value, List<String> stats, List<String> profs) {
+  String statistic = '';
+  String modifier = '';
+
+  if (value < 10) {
+    statistic = '0';
+  }
+
+  modifier = valueToSignedString(modifierFromValue(value));
+
+  statistic = statistic + value.toString();
+
   Widget returnBox = Card(
     child: Padding(
       padding: const EdgeInsets.all(8.0),
       child: Row(
         children: [
           Expanded(
-            flex: 2,
+            flex: 4,
             child: Column(
               children: [
-                Expanded(
-                  flex: 8,
-                  child: FittedBox(
-                    fit: BoxFit.fitWidth,
-                    child: Text(value.toString()),
-                  ),
-                ),
                 Expanded(
                   flex: 3,
                   child: FittedBox(
@@ -89,41 +113,125 @@ Widget attributeBoxGenerator(
                     child: Text(name),
                   ),
                 ),
+                Expanded(
+                  flex: 5,
+                  child: FittedBox(
+                    fit: BoxFit.fitHeight,
+                    child: Text(modifier),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: FittedBox(
+                    fit: BoxFit.fitHeight,
+                    child: Card(child: Text(statistic)),
+                  ),
+                )
               ],
             ),
           ),
-          const Divider(),
+          const Expanded(
+            flex: 1,
+            child: VerticalDivider(),
+          ),
           Expanded(
-            flex: 3,
+            flex: 5,
             child: Column(
               children: [
                 Expanded(
                   flex: 1,
-                  child: Text(stats[0], textAlign: TextAlign.left),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(stats[0]),
+                  ),
+                ),
+                const Divider(),
+                Expanded(
+                  flex: 1,
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(stats[1]),
+                  ),
                 ),
                 Expanded(
                   flex: 1,
-                  child: Text(stats[1], textAlign: TextAlign.left),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(stats[2]),
+                  ),
                 ),
                 Expanded(
                   flex: 1,
-                  child: Text(stats[2], textAlign: TextAlign.left),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(stats[3]),
+                  ),
                 ),
                 Expanded(
                   flex: 1,
-                  child: Text(stats[3], textAlign: TextAlign.left),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(stats[4]),
+                  ),
                 ),
                 Expanded(
                   flex: 1,
-                  child: Text(stats[4], textAlign: TextAlign.left),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Text(stats[5], textAlign: TextAlign.left),
+                  child: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(stats[5]),
+                  ),
                 ),
               ],
             ),
-          )
+          ),
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Text(valueToSignedString(modifierFromValue(value) +
+                      profBonusFromLevel(level) * int.parse(profs[0]))),
+                ),
+                const Expanded(child: Text('')),
+                Expanded(
+                  flex: 1,
+                  child: Text(profs[1] == ''
+                      ? ''
+                      : valueToSignedString(modifierFromValue(value) +
+                          profBonusFromLevel(level) * int.parse(profs[1]))),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text(profs[2] == ''
+                      ? ''
+                      : valueToSignedString(modifierFromValue(value) +
+                          profBonusFromLevel(level) * int.parse(profs[2]))),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text(profs[3] == ''
+                      ? ''
+                      : valueToSignedString(modifierFromValue(value) +
+                          profBonusFromLevel(level) * int.parse(profs[3]))),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text(profs[4] == ''
+                      ? ''
+                      : valueToSignedString(modifierFromValue(value) +
+                          profBonusFromLevel(level) * int.parse(profs[4]))),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Text(profs[5] == ''
+                      ? ''
+                      : valueToSignedString(modifierFromValue(value) +
+                          profBonusFromLevel(level) * int.parse(profs[5]))),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     ),
@@ -141,47 +249,69 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int currIndex = 0;
 
-  static const String nomePersonaggio = 'Pallad';
-
   final drawerItems = ListView();
 
   @override
   Widget build(BuildContext context) {
     List<Widget> attributes = [];
 
-    attributes.add(attributeBoxGenerator(attributeNames[0], 1,
-        ['Saving Throws', 'Athletics', '', '', '', ''], []));
+    attributes.add(attributeBoxGenerator(
+        attributeNames[0],
+        10,
+        ['Saving Throws', '', '', '', '', 'Athletics'],
+        ['1', '', '', '', '', '0']));
     attributes.add(attributeBoxGenerator(
         attributeNames[1],
-        2,
-        ['Saving Throws', 'Acrobatics', 'Sleight of Hand', 'Stealth', '', ''],
-        []));
-    attributes.add(attributeBoxGenerator(
-        attributeNames[2], 3, ['Saving Throws', '', '', '', '', ''], []));
-    attributes.add(attributeBoxGenerator(attributeNames[3], 4, [
+        08,
+        ['Saving Throws', '', '', 'Acrobatics', 'Sleight of Hand', 'Stealth'],
+        ['0', '', '', '1', '0', '2']));
+    attributes.add(attributeBoxGenerator(attributeNames[2], 16,
+        ['Saving Throws', '', '', '', '', ''], ['0', '', '', '', '', '']));
+    attributes.add(attributeBoxGenerator(attributeNames[3], 12, [
       'Saving Throws',
       'Arcana',
       'History',
       'Investigation',
       'Nature',
       'Religion'
-    ], []));
-    attributes.add(attributeBoxGenerator(attributeNames[4], 5, [
+    ], [
+      '1',
+      '0',
+      '0',
+      '0',
+      '0',
+      '0'
+    ]));
+    attributes.add(attributeBoxGenerator(attributeNames[4], 15, [
       'Saving Throws',
       'Animal Handling',
       'Insight',
       'Medicine',
       'Perception',
       'Survival'
-    ], []));
-    attributes.add(attributeBoxGenerator(attributeNames[5], 6, [
+    ], [
+      '0',
+      '2',
+      '0',
+      '1',
+      '0',
+      '0'
+    ]));
+    attributes.add(attributeBoxGenerator(attributeNames[5], 14, [
       'Saving Throws',
+      '',
       'Deception',
       'Intimidation',
       'Performance',
-      'Persuasion',
-      ''
-    ], []));
+      'Persuasion'
+    ], [
+      '0',
+      '',
+      '0',
+      '0',
+      '2',
+      '0'
+    ]));
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 //------------------------------------------------------------------------------  HOME  ------------------------------------------------------------------------------------
@@ -228,59 +358,64 @@ class _MyAppState extends State<MyApp> {
           ),
           Expanded(
             flex: 6,
-            child: ListView(
-              children: const [
-                Text('data01'),
-                Text('data02'),
-                Text('data03'),
-                Text('data04'),
-                Text('data05'),
-                Text('data06'),
-                Text('data07'),
-                Text('data08'),
-                Text('data09'),
-                Text('data10'),
-                Text('data11'),
-                Text('data12'),
-                Text('data13'),
-                Text('data14'),
-                Text('data15'),
-                Text('data16'),
-                Text('data17'),
-                Text('data18'),
-                Text('data19'),
-                Text('data20'),
-                Text('data21'),
-                Text('data22'),
-                Text('data23'),
-                Text('data24'),
-                Text('data25'),
-                Text('data26'),
-                Text('data27'),
-                Text('data28'),
-                Text('data29'),
-                Text('data30'),
-                Text('data31'),
-                Text('data32'),
-                Text('data33'),
-                Text('data34'),
-                Text('data35'),
-                Text('data36'),
-                Text('data37'),
-                Text('data38'),
-                Text('data39'),
-                Text('data40'),
-                Text('data41'),
-                Text('data42'),
-                Text('data43'),
-                Text('data44'),
-                Text('data45'),
-                Text('data46'),
-                Text('data47'),
-                Text('data48'),
-                Text('data49'),
-                Text('data50'),
-              ],
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView(
+                  children: const [
+                    Text('data01'),
+                    Text('data02'),
+                    Text('data03'),
+                    Text('data04'),
+                    Text('data05'),
+                    Text('data06'),
+                    Text('data07'),
+                    Text('data08'),
+                    Text('data09'),
+                    Text('data10'),
+                    Text('data11'),
+                    Text('data12'),
+                    Text('data13'),
+                    Text('data14'),
+                    Text('data15'),
+                    Text('data16'),
+                    Text('data17'),
+                    Text('data18'),
+                    Text('data19'),
+                    Text('data20'),
+                    Text('data21'),
+                    Text('data22'),
+                    Text('data23'),
+                    Text('data24'),
+                    Text('data25'),
+                    Text('data26'),
+                    Text('data27'),
+                    Text('data28'),
+                    Text('data29'),
+                    Text('data30'),
+                    Text('data31'),
+                    Text('data32'),
+                    Text('data33'),
+                    Text('data34'),
+                    Text('data35'),
+                    Text('data36'),
+                    Text('data37'),
+                    Text('data38'),
+                    Text('data39'),
+                    Text('data40'),
+                    Text('data41'),
+                    Text('data42'),
+                    Text('data43'),
+                    Text('data44'),
+                    Text('data45'),
+                    Text('data46'),
+                    Text('data47'),
+                    Text('data48'),
+                    Text('data49'),
+                    Text('data50'),
+                  ],
+                ),
+              ),
             ),
           ),
         ],
@@ -292,16 +427,51 @@ class _MyAppState extends State<MyApp> {
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     Widget inventory = Scaffold(
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(10),
-          color: Colors.blue,
-          height: 100,
-          width: 100,
-          child: const Text('inventory'),
-          //decoration: BoxDecoration(shape: BoxShape.circle),
-        ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Card(
+                    color: Colors.blue,
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
+                      height: 100,
+                      width: 100,
+                      child: const Text('Inventory'),
+                      //decoration: BoxDecoration(shape: BoxShape.circle),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Card(
+                    color: Colors.blue,
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
+                      height: 100,
+                      width: 100,
+                      child: const Text('Inventory'),
+                      //decoration: BoxDecoration(shape: BoxShape.circle),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
 
@@ -310,16 +480,51 @@ class _MyAppState extends State<MyApp> {
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
     Widget spells = Scaffold(
-      body: Center(
-        child: Container(
-          margin: const EdgeInsets.all(10),
-          padding: const EdgeInsets.all(10),
-          color: Colors.red,
-          height: 100,
-          width: 100,
-          child: const Text('spells'),
-          //decoration: BoxDecoration(shape: BoxShape.circle),
-        ),
+      body: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Card(
+                    color: Colors.red,
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
+                      height: 100,
+                      width: 100,
+                      child: const Text('spells'),
+                      //decoration: BoxDecoration(shape: BoxShape.circle),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Card(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Card(
+                    color: Colors.red,
+                    child: Container(
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
+                      height: 100,
+                      width: 100,
+                      child: const Text('spells'),
+                      //decoration: BoxDecoration(shape: BoxShape.circle),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
 
@@ -347,8 +552,6 @@ class _MyAppState extends State<MyApp> {
           activeIcon: Icon(Icons.auto_fix_high_rounded, color: Colors.grey)),
     ];
 
-    assert(tabPages.length == barItems.length);
-
     final bottNavBar = BottomNavigationBar(
       items: barItems,
       fixedColor: Colors.grey,
@@ -368,7 +571,7 @@ class _MyAppState extends State<MyApp> {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.grey[650],
-          title: const Text(nomePersonaggio),
+          title: Text(nomePersonaggio),
         ),
         body: Container(
           padding: const EdgeInsets.all(15),
